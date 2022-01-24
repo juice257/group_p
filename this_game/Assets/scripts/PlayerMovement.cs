42041b8302e34538093f1baf15d04ee6b8e5d7a8
FARHAN KALAM <8000343@cpsd.us>
2022-01-24 12:44:31 -0500

    updated

--- this_game/Assets/scripts/PlayerMovement.cs
+++ this_game/Assets/scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector2 change;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
