262ee89371fff3f72feea29e54b2d9d63446f047
JUSTIN-CORI AZEVEDO <2036157@cpsd.us>
2022-01-31 13:27:47 -0500

    updated for gfix to log.cs

--- project/Myproject/Assets/scripts/Log.cs
+++ project/Myproject/Assets/scripts/Log.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Log : Enemy {
+public class Log : Enemy
+{
 
     private Rigidbody2D myRigidbody;
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
     public Transform Origin;
+    public Animator anim;
+
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         currentState = EnemyState.idle;
         myRigidbody = GetComponent<Rigidbody2D>();
         target = GameObject.FindWithTag("Player").transform;
@@ -23,19 +27,36 @@ public class Log : Enemy {
         CheckDistance();
     }
 
-    void CheckDistance() {
-        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) {
-            if(currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger) {
-                Vector3 temp = Vector3.MoveTowards(transform.position, 
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position,
                 target.position, moveSpeed * Time.deltaTime);
                 myRigidbody.MovePosition(temp);
+                changeAnim(temp - transform.position);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
             }
+        }
+        else
+        {
+
+            anim.SetBool("wakeup", false);
         }
     }
 
-    private void ChangeState(EnemyState newState) {
-        if(currentState != newState) {
+
+    private void changeAnim(Vector2 direction)
+    {
+        Debug.Log(direction);
+    }
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState != newState)
+        {
             currentState = newState;
         }
     }
