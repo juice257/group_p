4563cd12885b4813ad3c554b41125a7db7eb04ba
JUSTIN-CORI AZEVEDO <2036157@cpsd.us>
2022-02-08 12:56:06 -0500

    part 21

--- project/Myproject/Assets/scripts/Knockback.cs
+++ project/Myproject/Assets/scripts/Knockback.cs
@@ -8,24 +8,14 @@ public class Knockback : MonoBehaviour {
     public float knockTime;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.CompareTag("breakable") && this.gameObject.CompareTag("Player")) {
-            other.GetComponent<Pot>().Smash();
-        }
-        if(other.gameObject.CompareTag("enemy") || other.gameObject.CompareTag("Player")) {
-            Rigidbody2D hit = other.GetComponent<Rigidbody2D>();
-            if(hit != null) {
-                Vector2 difference = hit.transform.position - transform.position;
+        if(other.gameObject.CompareTag("enemy")) {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if(enemy != null) {
+                enemy.isKinematic = false;
+                Vector2 difference = enemy.transform.position - transform.position;
                 difference = difference.normalized * thrust;
-                hit.AddForce(difference, ForceMode2D.Impulse);
-                if(other.gameObject.CompareTag("enemy")) {
-                    hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
-                    other.GetComponent<Enemy>().Knock(hit, knockTime);
-                }
-                if(other.gameObject.CompareTag("Player")) {
-                    hit.GetComponent<Movement>().currentState = PlayerState.stagger;
-                    other.GetComponent<Movement>().Knock(knockTime);
-                }
-                
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
             }
         }
     }
@@ -35,7 +25,7 @@ public class Knockback : MonoBehaviour {
         if(enemy != null) {
             yield return new WaitForSeconds(knockTime);
             enemy.velocity = Vector2.zero;
-            enemy.GetComponent<Enemy>().currentState = EnemyState.idle;
+            enemy.isKinematic = true;
         }
     } 
 }
